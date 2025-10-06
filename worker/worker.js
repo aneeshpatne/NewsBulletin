d06f889d5c0f9c01870d31c7df277357dd6337e0
Aneesh <aneeshpatne@gmail.com>
2025-10-06 13:12:09 +0000

    Refactor worker.js to add delay hours functionality and integrate task queue for job processing

--- worker/worker.js
+++ worker/worker.js
@@ -1,10 +1,21 @@
 import { Worker } from "bullmq";
 import { connection } from "../lib/redis";
+import { taskQueue } from "../queues/taskqueue";
+let delayHours = 3;
 
+export function setDelayHours(newVal) {
+  delayHours = newVal;
+  console.log("[SCHEDULER] New Delay Value set as: ", newVal);
+}
 new Worker(
   "tasks",
   async (job) => {
     console.log("Job Started");
+    await taskQueue.add(
+      "tasks",
+      { prev: job.id },
+      { delay: nextDelay, removeOnComplete: true }
+    );
     return { done: true };
   },
   { connection }
