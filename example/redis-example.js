e4165aeff795d730747e4df479f7fbbf0eb9c6f9
Aneesh <aneeshpatne@gmail.com>
2025-10-07 11:22:15 +0000

    Add redis example script for news item storage and retrieval

---
+++ example/redis-example.js
@@ -0,0 +1,22 @@
+import { client } from "../lib/redis_old.js";
+
+await client.rPush(
+  "news_item_new",
+  JSON.stringify({
+    Title: "Maha Issues Custodial Death Guidelines",
+    description:
+      "Maharashtra issues investigation guidelines in Somnath Suryawanshi custodial death case to probe custodial deaths rigorously.",
+  })
+);
+
+const items = await client.lRange("news_item_new", 0, -1);
+
+const parsed = items.map((it) => {
+  try {
+    return JSON.parse(it);
+  } catch (e) {
+    return it;
+  }
+});
+
+console.log(parsed);
